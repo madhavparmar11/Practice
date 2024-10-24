b2f7b132cb07db7e5057304b503d855f0d95577c
madhavparmar11 <152685058+madhavparmar11@users.noreply.github.com>
2024-10-24 11:45:20 +0530

    Add files via upload

---
+++ main 2.py
@@ -0,0 +1,16 @@
+n1=int (input("write a number: "))
+n2=int (input("write a number: "))
+n3=int (input("write a number: "))
+if(n1>n2 or n1>n3):
+    print("n1 is greater")
+if(n2>n1 or n2>n3):
+    print("n2 is greater")
+if(n3>n1 or n3>n2):
+    print("n3 is greater")
+
+else:
+    print("recheck")
+#if(num%2==0):
+ #   print("EVEN")
+#else:
+ #   print("NOT EVEN")
---
+++ main 4.py
@@ -0,0 +1,5 @@
+num=int (input("write a number: "))
+if(num%7==0):
+    print("MULTIPLE")
+else:
+    print("NOT MULTIPLE")
