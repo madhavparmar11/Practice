7795138afd06069028332f66d5669fdb3d7f4b39
madhavparmar11 <152685058+madhavparmar11@users.noreply.github.com>
2024-10-26 11:56:13 +0530

    Add files via upload

---
+++ main 10.py
@@ -0,0 +1,4 @@
+a={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(type(a))
+print("THE REQUIRED CLASSES ARE:")
+print(len(a))
---
+++ main 11.py
@@ -0,0 +1,7 @@
+dict={"name": "madhav",
+        "phy":"85",
+        "chm": "85",
+        "maths":"85"
+    }
+    
+print (list(dict))
